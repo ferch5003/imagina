4a32d786f8eab11be0779c95d01f426c296cb7de
Willian Garcia <Wbovea@uninorte.edu.co>
2019-04-27 13:08:22 -0500

    First commit

---
+++ components/TarjetaPropuesta.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="box  is-one-quarter column">
+    <h3>{{ propuesta.title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TarjetaPropuesta',
+  props: {
+    propuesta: Object
+  }
+}
+</script>
+
+<style scope>
+.box{
+
+  margin:1em;
+}
+</style>
---
+++ pages/busqueda.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="container">
+  <div>
+    <h1 class="title">Consolidado de Propuestas</h1>
+      <div class="columns is-multiline">
+       <TarjetaPropuesta :propuesta="propuesta" v-for="propuesta in propuestas" :key= "propuesta.id" />
+      </div>
+  </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import env from '../config/env.js'
+import TarjetaPropuesta from '../components/TarjetaPropuesta.vue'
+export default {
+  name: 'SearchPage',
+  components: {
+    TarjetaPropuesta
+  },
+  data() {
+    return {
+      propuestas: []
+    }
+  },
+  created() {
+    axios.get(`${env.endpoint}/posts`).then((response) => {
+      this.propuestas = response.data
+    })
+  }
+}
+</script>
+
+<style scope>
+
+</style>
